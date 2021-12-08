18a2bd4d50863b9419121e6376782ac9f414d2d1
Praetz <michaelpraetz7@gmail.com>
2021-12-08 10:45:37 -0700

    making button

---
+++ button/src/components/buttn/buttn.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../buttn/buttn.jsx';
+
+const name = showText
+
+function buttn() {
+    return (
+        <div>
+            <p class="name">freeCodeCamp</p>
+            <button onclick='showText'>Change to Blue</button>
+        </div>
+
+    );
+
+}
+
+
